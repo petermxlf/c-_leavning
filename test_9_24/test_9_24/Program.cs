07ad07eeb432fa5f811b0255a954b300dc8f469e
petermxlf <133092645+petermxlf@users.noreply.github.com>
2023-09-25 19:05:38 +0800

    Add files via upload

---
+++ test_9_24/test_9_24/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace test_9_24
+{
+    using System;
+
+    internal class Program
+    {
+        //class BoxingDome
+        //{
+        //    static void Main()
+        //    {
+        //        Console.WriteLine("执行装箱转换:");
+        //        int k = 200;
+        //        object obj = k;
+        //        k = 300;
+        //        Console.WriteLine("k={0}", k);//300
+        //        Console.WriteLine("obj={0}",obj);//200
+                
+        //    }
+        //}
+        //class UnboxingDemo
+        //{
+        //    static void Main()
+        //    {
+        //        int k = 228;
+        //        object obj = k;//装箱转换
+        //        int j = (int)obj;//拆箱转换
+        //        Console.WriteLine("k={0}\tobj={1}\tj={2}", k, obj, j);
+        //    }
+        //}
+        //class Const//常量与变量
+        //{
+        //    static void Main()
+        //    {
+        //        int a = 10;
+        //        const int b = 20;
+        //        //const int c = a + 20;//错误，常量表达式不能包含变量
+        //        const int d = b + 20;//正确，b是常量
+        //        //b = 100;//错误,常量不可以改变
+
+        //    }
+        //}
+        class main1//运算符
+        {
+            static void Main()
+            {
+                //int a = 100;
+                //int x = 20;
+                //int y = 30;
+                //int b;
+                //b = x + y;
+                //bool j = a > b;
+                //Console.WriteLine("a>b={0}", j);//True
+                //00000000000000000000000001100100
+                //11111111111111111111111110011011//补码
+                //10000000000000000000000001100100//反码
+                //10000000000000000000000001100101//原码
+                //Console.WriteLine("a 取反后:{0}", ~a);//-101
+                //Console.WriteLine("a 右移2位:{0}", a>>2);//25
+                //string str = "hello" + " " + "world!";
+                //Console.WriteLine(str);
+                //bool istest = x is int;//is运算符判断表达式是否是指定类型
+                //Console.WriteLine(istest);//true
+                //Console.WriteLine(sizeof(int) * 10 / 3.2);//12.5
+                //Console.WriteLine(8>6+3);//false
+                //Console.WriteLine(5-2<=3);//true
+                //Console.WriteLine(6>2&&8>9);//false
+                //Console.WriteLine("Computer"=="Games");//false
+                //Console.WriteLine("hello "+"world");//hello world
+                //Console.WriteLine(@"This is "+"a book "!="This is \\ "+"a book");//true
+                //bool x = true, y = true, c = false;
+                //int a = 6;
+                //Console.WriteLine(!x||y&&y||c);
+                //Console.WriteLine(x&&6<=9||a>=7&&c);
+                //在c#中用const定义常量，常量定义时必须初始化，因为常量是不可修改的
+
+                //循序结构
+                //const double PI = 3.14;
+                //double r, l, s;
+                //Console.WriteLine("请输入园的半径:");
+                //r = double.Parse(Console.ReadLine());
+                //l = 2 * PI * r;
+                //s = PI * r * r;
+                //Console.WriteLine(l);
+                //Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ welcome1/welcome1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace welcome1
+{
+        using System;//使用using语句导入system命名空间中
+    internal class Program
+    {
+    class welcome1//类
+    {
+        static void Main(string[] args)
+        {
+                //System.Console.WriteLine("欢迎使用c#\n");
+                //Console.WriteLine("欢迎使用c#\n");//因为前面使用using导入命名空间，所以可以不用System
+                //double k = 1234.789;
+                //Console.WriteLine("{0,8:c}",k);//货币形式输出
+                //Console.Write("{0,8:c}",k);//货币形式输出
+                //Console.WriteLine("{0,10:c3}",k);
+                //Console.Write("{0,10:c3}\n",k);
+                string str;
+                Console.WriteLine("请输入你的名字>");
+                str= Console.ReadLine();
+                Console.WriteLine("{0},欢迎你", str);
+        }
+    }
+        
+    }
+}
