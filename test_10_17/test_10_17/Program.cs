9706c89f220d351b220454287120aa69e6da003b
mxlf <133092645+petermxlf@users.noreply.github.com>
2023-10-17 22:27:06 +0800

    Add files via upload

---
+++ test_10_17/test_10_17/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_10_17
+{
+    //internal class Program
+    //{
+    //    delegate int Mydelegate();//声明委托
+    //    class MyMesh
+    //    {
+    //        public int M1()
+    //        {
+    //            Console.WriteLine("调用实例方法");
+    //            return 0;
+    //        }
+    //        public static int M2()
+    //        {
+    //            Console.WriteLine("调用静态方法");
+    //            return 0;
+    //        }
+    //    }
+    //    class Test
+    //    {
+    //        static void Main(string[] args)
+    //        {
+    //            //MyMesh p = new MyMesh();
+    //            //Mydelegate m = new Mydelegate(p.M1);//调用实例成员
+    //            //m();
+    //            //Mydelegate m2 = new Mydelegate(MyMesh.M2);//调用静态方法成员
+    //            //m2();
+    //            //在委托对象中封装多个方法
+    //            MyMesh m = new MyMesh();
+    //            Mydelegate a = new Mydelegate(m.M1);
+    //            Mydelegate b = new Mydelegate(MyMesh.M2);
+    //            Mydelegate c = a + b;
+    //            c();//同时调用两个方法
+    //            c -= a;
+    //            c -= b;
+    //        }
+    //    }
+    //}
+    class Test
+    {
+        //声明事件
+        //修饰符 event 委托名 事件名；
+    }
+}
