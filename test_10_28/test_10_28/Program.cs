4733f556934ff2a9875d5ce43b84b4bf2a4bfe68
mxlf <133092645+petermxlf@users.noreply.github.com>
2023-10-28 21:00:42 +0800

    Add files via upload

---
+++ test_10_28/test_10_28/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_10_28
+{
+    using System;
+    //抽象类与接口
+    //接口的实现
+    /*interface Imethods
+    {
+        void F();
+        void G();
+    }
+    abstract class C : Imethods
+    {
+        public abstract void F();
+        public abstract void G();
+    }*/
+    //对抽象方法的实现
+    /*interface Imethods
+    {
+        void F();
+        void G();
+    }
+    abstract class C : Imethods
+    {
+        void Imethods.F() { FF(); }
+        void Imethods.G() { GG(); }
+        protected abstract void FF();
+        protected abstract void GG();
+    }*/
+}
