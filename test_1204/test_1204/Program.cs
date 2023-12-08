9ec4b82792902ef4b5773f53503c7c33a588933b
mxlf <133092645+petermxlf@users.noreply.github.com>
2023-12-08 21:18:24 +0800

    Add files via upload

---
+++ test_1204/test_1204/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_1204
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double df;
+            int n;
+            df = n = 100;
+        }
+    }
+}
